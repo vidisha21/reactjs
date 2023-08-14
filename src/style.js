22c4fc71a985a48540791501ec3c14c870c4c019
navin-pareta <141991630+navin-pareta@users.noreply.github.com>
2023-08-14 20:29:57 +0530

    Update style.js

--- src/style.js
+++ src/style.js
@@ -5,7 +5,7 @@ display: flex;
 flex-direction: column;
 justifyContent : center;
  alignItems : center;
-width: 100%;
+width: 50%;
 height: 100%;
 background : url('https://img.freepik.com/premium-photo/soccer-stadium-evening-arena-with-crowd-fans-d-illustration_336913-373.jpg?size=626&ext=jpg&ga=GA1.1.1522961941.1691787232&semt=ais');
 background-repeat: no-repeat;
