12b763688521fa21845eaca608c9915200d9a9d8
vidisha21 <vidishamamnani921@gmail.com>
2023-08-18 08:46:45 +0530

    styling

---
+++ src/component/header/style.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+const Wrapper = styled.header`
+background : #ff2c2c;
+color : white;
+text-align: center;
+padding : 12px;
+`
+export default Wrapper
--- src/style.js
+++ src/style.js
@@ -2,47 +2,50 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    
-max-width: 1068px;
-margin: auto;
-width: 100%;
-padding-bottom: 2%;
-display: flex;
-flex-direction: column;
-align-items: center; 
-justify-content: center; 
+ position : absolute;
+ bottom : 0px;
+ right : 0px;
+ left :0px;
+ border : solid 2px black;
+ 
+
 `;
 
 export const ScoreButton = styled.button`
   display: inline-block;
-  width: 65px; 
-  height: 47px; 
-  margin: 4px; 
-  background-color: teal;
-  color: white;
-  border: solid 2px grey;
-  border-radius: 3px;
+  width: 100px; 
+  height: 110px; 
+  background-color: white;
+  color: black;
+  border: solid 0.9px grey;
+  // border-radius: 1px;
   cursor: pointer;
+  font-size: 22px;
+  box-sizing : border-box;
+  background : #d3d3d3;
+  flex : 1;
 
   &:hover {
     background-color: grey;
   }
 `;
+export const Background = styled.div`
+border : solid 4px black;`
 
-export const ResetButton = styled.button`
-  display: block;
-  margin: 10px auto; 
-  padding: 10px 20px;
-  background-color: teal;
-  color: white;
-  border: none;
-  border-radius: 3px;
-  cursor: pointer;
+// export const ResetButton = styled.button`
+//   display: block;
+//   margin: 10px auto; 
+//   padding: 10px 20px;
+//   background-color: teal;
+//   color: white;
+//   border: none;
+//   border-radius: 3px;
+//   cursor: pointer;
 
-  &:hover {
-    background-color: grey;
-  }
-`;
+//   &:hover {
+//     background-color: grey;
+//   }
+// `;
 // export const Whole = styled.div`
 // background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOK6MKUnKzFRD7d9WBgKv9OT8YIBNsKHsutPAdXYHWWg&s' style={{width:'500px'}} );
 //   background-repeat: no-repeat;`;
